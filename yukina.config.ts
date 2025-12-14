14bb72e5e59482289895899347df1d7443f8afeb
marxhack <114662618+marxhack@users.noreply.github.com>
2025-12-14 22:51:22 +0000

    x

--- yukina.config.ts
+++ yukina.config.ts
@@ -53,9 +53,9 @@ const YukinaConfig: Configuration = {
   maxFooterTagChip: 24,
 
   banners: [
+    "https://interoperable-europe.ec.europa.eu/sites/default/files/styles/wysiwyg_full_width/public/inline-images/BugBounty.png?itok=Z322FJ3w",
     "https://www.tripwire.com/sites/default/files/CTF.jpg",
     "https://cdn.nulab.com/learn-wp/app/uploads/2019/05/14210442/Nulab-Capture-the-Flag-CTF-Challenge-Blog.png",
-    "https://interoperable-europe.ec.europa.eu/sites/default/files/styles/wysiwyg_full_width/public/inline-images/BugBounty.png?itok=Z322FJ3w",
     "https://ctftime.org/media/cache/eb/47/eb47a9046e2125339a21f474e2bf44ba.png",
     "https://s2.loli.net/2025/01/25/Pm89OveZq7NWUxF.webp",
     "https://s2.loli.net/2025/01/25/UCYKvc1ZhgPHB9m.webp",
