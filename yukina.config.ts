c6816b9c31b367084e73f0da45e42c13057c476c
marxhack <114662618+marxhack@users.noreply.github.com>
2025-12-14 22:40:16 +0000

    x

--- yukina.config.ts
+++ yukina.config.ts
@@ -54,7 +54,7 @@ const YukinaConfig: Configuration = {
 
   banners: [
     "https://images.unsplash.com/photo-1649451844897-8699a1d6d96d?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    "https://s2.loli.net/2025/01/25/6bKcwHZigzlM4mJ.webp",
+    "https://www.tripwire.com/sites/default/files/CTF.jpg",
     "https://s2.loli.net/2025/01/25/H9WgEK6qNTcpFiS.webp",
     "https://s2.loli.net/2025/01/25/njNVtuUMzxs81RI.webp",
     "https://s2.loli.net/2025/01/25/tozsJ8QHAjFN3Mm.webp",
