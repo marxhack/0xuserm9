300500cce04108395f046a10bf8fefc12de6893d
marxhack <114662618+marxhack@users.noreply.github.com>
2025-12-14 23:41:56 +0000

    X

--- yukina.config.ts
+++ yukina.config.ts
@@ -69,7 +69,7 @@ const YukinaConfig: Configuration = {
   },
 
   // WIP functions
-  bannerStyle: "STATIC", // 'loop' | 'static' | 'hidden'
+  bannerStyle: "hidden", // 'loop' | 'static' | 'hidden'
 };
 
 export default YukinaConfig;
