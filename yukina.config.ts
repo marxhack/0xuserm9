1aceea2d2b087740f0b5d6d34e1d4493cae65a6c
marxhack <114662618+marxhack@users.noreply.github.com>
2025-12-15 14:21:18 +0000

    x

--- yukina.config.ts
+++ yukina.config.ts
@@ -64,8 +64,8 @@ const YukinaConfig: Configuration = {
   slugMode: "RAW", // 'RAW' | 'HASH'
 
   license: {
-    name: "CC BY-NC-SA 4.0",
-    url: "https://creativecommons.org/licenses/by-nc-sa/4.0/",
+    name: "",
+    url: "",
   },
 
   // WIP functions
