b7ba78e5a59e00e6f5123bb6bf65ff7fd860f047
marxhack <114662618+marxhack@users.noreply.github.com>
2026-03-19 21:33:47 +0000

    xx

--- src/locales/languages/en.ts
+++ src/locales/languages/en.ts
@@ -9,7 +9,7 @@ export const en: Translation = {
   [key.nav_bar_home]: "Home",
   [key.nav_bar_archive]: "Archive",
   [key.nav_bar_about]: "About",
-  [key.nav_bar_github]: "GitHub",
+  [key.nav_bar_medium]: "Medium",
   [key.nav_bar_search_placeholder]: "Search",
 
   [key.post_card_words]: "Words",
