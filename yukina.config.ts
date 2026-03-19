d0452f4d174901428592fceb0f65785657773744
marxhack <114662618+marxhack@users.noreply.github.com>
2026-03-19 21:23:05 +0000

    x

--- yukina.config.ts
+++ yukina.config.ts
@@ -25,7 +25,7 @@ const YukinaConfig: Configuration = {
       href: "/about",
     },
     {
-      nameKey: I18nKeys.nav_bar_github,
+      nameKey: I18nKeys.nav_bar_medium,
       href: "https://medium.com/@0xuserm9",
     },
   ],
